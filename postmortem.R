e709572104dfdb1c51e9ccb25a45ac496f7ad8e6
gabriel.oana <gabriel.oana@outplay.com>
2017-05-18 19:22:48 +0100

    test

--- postmortem.R
+++ postmortem.R
@@ -1,30 +1,4 @@
-########################################################
-####### Get Revenue over a range of days ###############
-########################################################
 
-
-start_date = "2017-05-12"
-end_date = "2017-05-14"
-
-
-source('~/amplitude-dashboard/dashboard-code/SharedConfig.R')
-
-temp <- amplitude("CT", event="unverified_revenue",
-                  group_by_properties=list(list(type="event", value="$revenue"), list(type="event", value="$revenueType")),
-                  measured_by="sums", start=start_date, end=end_date)
-
-names(temp)[names(temp)=="property"] <- "code"
-
-
-conv <- get_exchange_rates(unique(temp$code), toCurr="USD", fromDt=min(temp$date), toDt=max(temp$date))
-
-revenue <- inner_join(temp, conv, by=c("code", "date"))
-revenue$revenue <- revenue$value * revenue$exch 
-
-rm(conv,temp)
-
-total_revenue <- sum(revenue$revenue, na.rm = T)
-print(total_revenue)  
 ########################################################
 ####### Get Revenue over a range of days ###############
 ########################################################

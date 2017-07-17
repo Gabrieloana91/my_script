cdcef743cee1f79182f181858a13fa78d63acc78
Gabriel Oana <gabriel.oana91@gmail.com>
2017-07-17 21:54:59 +0100

    Changes

--- r-lib2/R/function_library_gabz.R
+++ r-lib2/R/function_library_gabz.R
@@ -7,7 +7,7 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
   # Comparison before and after are expressed in days. Eg: 14 if you would like to compare the sale period to a simillar period 14 days ago. 
   # Comparison after is set for 7 days however, if there is no week ahead to compare with, all metrics are excluded over the "next week" period. 
   
-
+  
   # Parameters
   
   sale_duration <- as.numeric(sale_duration)
@@ -85,7 +85,7 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
                                  ifelse(date >= week_after_start & date <= week_after_end, "week_after", NA)))) %>% 
     rename(daily_spenders = value,
            unique_spenders = uniques_over_date_range)
-
+  
   
   if(as.Date(week_after_end) < Sys.Date()-1){
     
@@ -97,7 +97,7 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
                                    ifelse(date >= week_after_start & date <= week_after_end, "week_after", NA)))) %>% 
       rename(daily_spenders = value,
              unique_spenders = uniques_over_date_range)
-
+    
     spender <- bind_rows(spender, temp)
     
   }
@@ -108,7 +108,7 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
   metrics <- left_join(metrics, players, by="group")
   
   rm(players, revenue, spender)
-
+  
   ############################################# -> ARPDAU, ARPPU and Conversion Rate
   
   
@@ -122,7 +122,7 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
            conversion_rate = daily_spenders / dau)
   
   
-
+  
   
   
   
@@ -159,8 +159,8 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
     filter(!is.na(group)) %>% 
     summarise(ave_purchases_per_user = mean(totals)/mean(uniques)) %>% 
     left_join(., active, by=c("group","property")) 
-
-
+  
+  
   # Gem Investment #
   
   gem_data <- amplitude("CT", event="level_account", group_by_properties = list(list(type = "event", value = "energyboost_used")),
@@ -204,7 +204,7 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
            ave_play_on = play_on / dau,
            ave_gem_investment = (elixir + reinforcements + sentinel + play_on) / dau)
   
-
+  
   metrics <- gem_data %>%
     filter(!is.na(group)) %>% 
     group_by(group) %>% 
@@ -330,7 +330,7 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
     group_by(product_id, group) %>% 
     summarise(revenue = sum(revenue,na.rm = T))
   
-
+  
   p4 <- ggplot(temp, aes(product_id, revenue, fill=group)) +
     geom_bar(stat="identity", position="dodge", colour = "darkblue") +
     scale_fill_manual(values = c("chartreuse4","dodgerblue4","darkorange3"))+
@@ -378,3 +378,208 @@ do_sale_postmortem_CT <- function(sale_name, sale_date_start, sale_duration = "2
   
   
 }
+
+rquery.cormat<-function(x, type=c('lower', 'upper', 'full', 'flatten'), graph=TRUE, graphType=c("correlogram", "heatmap"), col=NULL, ...){
+  library(corrplot)
+  # Helper functions
+  #+++++++++++++++++
+  # Compute the matrix of correlation p-values
+  cor.pmat <- function(x, ...) {
+    mat <- as.matrix(x)
+    n <- ncol(mat)
+    p.mat<- matrix(NA, n, n)
+    diag(p.mat) <- 0
+    for (i in 1:(n - 1)) {
+      for (j in (i + 1):n) {
+        tmp <- cor.test(mat[, i], mat[, j], ...)
+        p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+      }
+    }
+    colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+    p.mat
+  }
+  # Get lower triangle of the matrix
+  getLower.tri<-function(mat){
+    upper<-mat
+    upper[upper.tri(mat)]<-""
+    mat<-as.data.frame(upper)
+    mat
+  }
+  # Get upper triangle of the matrix
+  getUpper.tri<-function(mat){
+    lt<-mat
+    lt[lower.tri(mat)]<-""
+    mat<-as.data.frame(lt)
+    mat
+  }
+  # Get flatten matrix
+  flattenCorrMatrix <- function(cormat, pmat) {
+    ut <- upper.tri(cormat)
+    data.frame(
+      row = rownames(cormat)[row(cormat)[ut]],
+      column = rownames(cormat)[col(cormat)[ut]],
+      cor  =(cormat)[ut],
+      p = pmat[ut]
+    )
+  }
+  # Define color
+  if (is.null(col)) {
+    col <- colorRampPalette(
+      c("#67001F", "#B2182B", "#D6604D", "#F4A582",
+        "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE", 
+        "#4393C3", "#2166AC", "#053061"))(200)
+    col<-rev(col)
+  }
+  
+  # Correlation matrix
+  cormat<-signif(cor(x, use = "complete.obs", ...),2)
+  pmat<-signif(cor.pmat(x, ...),2)
+  # Reorder correlation matrix
+  ord<-corrMatOrder(cormat, order="hclust")
+  cormat<-cormat[ord, ord]
+  pmat<-pmat[ord, ord]
+  # Replace correlation coeff by symbols
+  sym<-symnum(cormat, abbr.colnames=FALSE)
+  # Correlogram
+  if(graph & graphType[1]=="correlogram"){
+    corrplot(cormat, type=ifelse(type[1]=="flatten", "lower", type[1]),
+             tl.col="black", tl.srt=45,col=col,...)
+  }
+  else if(graphType[1]=="heatmap")
+    heatmap(cormat, col=col, symm=TRUE)
+  # Get lower/upper triangle
+  if(type[1]=="lower"){
+    cormat<-getLower.tri(cormat)
+    pmat<-getLower.tri(pmat)
+  }
+  else if(type[1]=="upper"){
+    cormat<-getUpper.tri(cormat)
+    pmat<-getUpper.tri(pmat)
+    sym=t(sym)
+  }
+  else if(type[1]=="flatten"){
+    cormat<-flattenCorrMatrix(cormat, pmat)
+    pmat=NULL
+    sym=NULL
+  }
+  list(r=cormat, p=pmat, sym=sym)
+}
+
+
+stat_smooth_func <- function(mapping = NULL, data = NULL,
+                             geom = "smooth", position = "identity",
+                             ...,
+                             method = "auto",
+                             formula = y ~ x,
+                             se = TRUE,
+                             n = 80,
+                             span = 0.75,
+                             fullrange = FALSE,
+                             level = 0.95,
+                             method.args = list(),
+                             na.rm = FALSE,
+                             show.legend = NA,
+                             inherit.aes = TRUE,
+                             xpos = NULL,
+                             ypos = NULL) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = StatSmoothFunc,
+    geom = geom,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      method = method,
+      formula = formula,
+      se = se,
+      n = n,
+      fullrange = fullrange,
+      level = level,
+      na.rm = na.rm,
+      method.args = method.args,
+      span = span,
+      xpos = xpos,
+      ypos = ypos,
+      ...
+    )
+  )
+}
+
+
+StatSmoothFunc <- ggproto("StatSmooth", Stat,
+                          
+                          setup_params = function(data, params) {
+                            # Figure out what type of smoothing to do: loess for small datasets,
+                            # gam with a cubic regression basis for large data
+                            # This is based on the size of the _largest_ group.
+                            if (identical(params$method, "auto")) {
+                              max_group <- max(table(data$group))
+                              
+                              if (max_group < 1000) {
+                                params$method <- "loess"
+                              } else {
+                                params$method <- "gam"
+                                params$formula <- y ~ s(x, bs = "cs")
+                              }
+                            }
+                            if (identical(params$method, "gam")) {
+                              params$method <- mgcv::gam
+                            }
+                            
+                            params
+                          },
+                          
+                          compute_group = function(data, scales, method = "auto", formula = y~x,
+                                                   se = TRUE, n = 80, span = 0.75, fullrange = FALSE,
+                                                   xseq = NULL, level = 0.95, method.args = list(),
+                                                   na.rm = FALSE, xpos=NULL, ypos=NULL) {
+                            if (length(unique(data$x)) < 2) {
+                              # Not enough data to perform fit
+                              return(data.frame())
+                            }
+                            
+                            if (is.null(data$weight)) data$weight <- 1
+                            
+                            if (is.null(xseq)) {
+                              if (is.integer(data$x)) {
+                                if (fullrange) {
+                                  xseq <- scales$x$dimension()
+                                } else {
+                                  xseq <- sort(unique(data$x))
+                                }
+                              } else {
+                                if (fullrange) {
+                                  range <- scales$x$dimension()
+                                } else {
+                                  range <- range(data$x, na.rm = TRUE)
+                                }
+                                xseq <- seq(range[1], range[2], length.out = n)
+                              }
+                            }
+                            # Special case span because it's the most commonly used model argument
+                            if (identical(method, "loess")) {
+                              method.args$span <- span
+                            }
+                            
+                            if (is.character(method)) method <- match.fun(method)
+                            
+                            base.args <- list(quote(formula), data = quote(data), weights = quote(weight))
+                            model <- do.call(method, c(base.args, method.args))
+                            
+                            m = model
+                            eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                                             list(a = format(coef(m)[1], digits = 3), 
+                                                  b = format(coef(m)[2], digits = 3), 
+                                                  r2 = format(summary(m)$r.squared, digits = 3)))
+                            func_string = as.character(as.expression(eq))
+                            
+                            if(is.null(xpos)) xpos = min(data$x)*0.9
+                            if(is.null(ypos)) ypos = max(data$y)*0.9
+                            data.frame(x=xpos, y=ypos, label=func_string)
+                            
+                          },
+                          
+                          required_aes = c("x", "y")
+)

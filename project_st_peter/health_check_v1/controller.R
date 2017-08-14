ad1289bb93ecb21c6d5406a3802993ca4f99dbae
gabriel.oana <gabriel.oana@outplay.com>
2017-08-14 09:05:41 +0100

    hcv1

---
+++ project_st_peter/health_check_v1/controller.R
@@ -0,0 +1,14 @@
+df <- fread("C:\\Users\\gabriel.oana\\Documents\\my_script\\project_st_peter\\health_check_v1\\data.csv")
+
+# 1. cleaning 
+
+names(df)[names(df) == "revenue_usd"] <- "revenue"
+
+df$level_id <- as.numeric(df$level_id)
+df$revenue <- as.numeric(df$revenue)
+df$revenue[is.na(df$revenue)] <- 0
+# Selecting only cohort_ids.
+
+df <- df %>% 
+  filter(cohort_id != ".")
+
